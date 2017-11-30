2d207083f89f954c0b24782e9ca8e6e7b04dc5fe
Harrison Kim <namo918@gmail.com>
2017-11-30 22:32:58 +0800

    work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-     resources :users, only: [ :index, :show ] do
-        resources :recordings, only: [ :index, :show ]
-        resources :votes, only: [ :index, :show ]
-      end
+     resources :users, :recordings, :votes, only: [ :index, :show ]
    end
  end
 end
