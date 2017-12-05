f66587364bcff90570fd7bd0447a31e13b274f37
Leon Lan <leolandongfang@gmail.com>
2017-12-05 16:13:39 +0800

    fixed testimonial pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129033727) do
+ActiveRecord::Schema.define(version: 20171204103839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20171129033727) do
     t.string   "description"
     t.string   "picture"
     t.string   "dialects"
+    t.string   "open_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
